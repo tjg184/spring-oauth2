350fde56f52b90bc17eeb3d51ed045e316d34cb9
Troy Gaines <tjg184@Troy-Gainess-MacBook-Pro.local>
2015-06-07 07:56:28 -0500

    Add a way to specify http method

--- oauth2/ui/src/main/java/demo/UiApplication.java
+++ oauth2/ui/src/main/java/demo/UiApplication.java
@@ -15,7 +15,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.security.oauth2.sso.EnableOAuth2Sso;
 import org.springframework.cloud.security.oauth2.sso.OAuth2SsoConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
@@ -33,15 +35,16 @@ public class UiApplication {
 	}
 
 	@Configuration
+	@Import(OAuth2SecureRouterConfiguration.class)
 	protected static class SecurityConfiguration extends OAuth2SsoConfigurerAdapter {
 
 		@Override
 		public void configure(HttpSecurity http) throws Exception {
+
 			http.logout().and().antMatcher("/**").authorizeRequests()
 					.antMatchers("/index.html", "/home.html", "/", "/login").permitAll()
-					.anyRequest().authenticated().and().csrf()
-					.csrfTokenRepository(csrfTokenRepository()).and()
-					.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+					.anyRequest().authenticated().and().csrf().disable()
+					.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		}
 
 		private Filter csrfHeaderFilter() {
